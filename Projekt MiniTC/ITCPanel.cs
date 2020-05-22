4b304296270288b4800f2b41dd91918e15b1e7a5
Piwo13 <63007989+Piwo13@users.noreply.github.com>
2020-05-22 22:41:34 +0200

    Add files via upload

---
+++ Projekt MiniTC/ITCPanel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Projekt_MiniTC
+{
+    public interface ITCPanel
+    {
+        string CurrPath { get; set; }
+        string[] Dyski { get; set; }
+        IEnumerable<object> PathCont { get; set; }
+    }
+}
---
+++ Projekt MiniTC/KopiaDanych.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Projekt_MiniTC
+{
+    static class KopiaDanych
+    {
+        public static void KopiujPliki(string _od, string _do)
+        {
+            FileInfo Od = new FileInfo(_od);
+            DirectoryInfo Do= new DirectoryInfo(_do);
+
+            string path = Do.FullName + "\\" + Od.Name;
+            int i = 2;
+
+            while (File.Exists(path))
+            {
+                path = Do.FullName + "\\" + Do.Name.Replace(Od.Extension, $"({i}){Od.Extension}");
+                i++;
+            }
+
+            File.Copy(Od.FullName, path);
+        }
+
+        public static void KopiujFolder(string _od,string _do)
+        {
+            DirectoryInfo Od = new DirectoryInfo(_od);
+            DirectoryInfo[] foldery = Od.GetDirectories();
+            FileInfo[] pliki = Od.GetFiles();
+
+            string path = _do + "\\" + Od.Name;
+            int i = 2;
+
+            while (Directory.Exists(path))
+            {
+                path = ($"{_do}\\{Od.Name} ({i})");
+                i++;
+            }
+
+            Directory.CreateDirectory(path);
+
+            foreach(FileInfo p in pliki)
+            {
+                File.Copy(p.FullName, path + "\\" + p.Name);
+            }
+
+            foreach(DirectoryInfo f in foldery)
+            {
+                KopiujFolder(f.FullName, path);
+            }
+        }
+    }
+}
